3fd02b6877926af2e1fbffffec6b306a9739727b
Rixhil <rishilshah0302@gmail.com>
2023-07-29 21:44:09 +0530

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -14,15 +14,6 @@ const renderApp = () => {
   );
 };
 
-// const renderApp = () => {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>,
-//     document.getElementById("root")
-//   );
-// };
-
 initFacebookSDK().then(() => {
   renderApp();
 });
